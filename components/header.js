da2d37a2d73a771fb984f8a9caf9d8b28cc2844f
Michel Manago <mmanago@kiolis.com>
2021-03-31 10:44:59 +0200

    New title for site

--- components/header.js
+++ components/header.js
@@ -15,7 +15,8 @@ const HeaderNavLink = ({label, href, subMenu}) => {
                         <button className={"uppercase px-3 py-3 text-xs block w-full "}>
                             <div className={"flex items-center " + (isSubMenu ? "pb-1" : "")}> 
                                 <span className="font-medium text-sm">{label}</span>
-                                <svg className="inline-block ml-auto" xmlns="http://www.w3.org/2000/svg" width="23.616" height="7" viewBox="0 0 23.616 13.503"><path fill="black" d="M18,20.679l8.93-8.937a1.681,1.681,0,0,1,2.384,0,1.7,1.7,0,0,1,0,2.391L19.2,24.258a1.685,1.685,0,0,1-2.327.049L6.68,14.14a1.688,1.688,0,0,1,2.384-2.391Z" transform="translate(-6.188 -11.246)"/></svg>
+                                <svg className="inline-block ml-auto" xmlns="http://www.w3.org/2000/svg" width="23.616" height="7" viewBox="0 0 23.616 13.503">
+                                    <path fill="black" d="M18,20.679l8.93-8.937a1.681,1.681,0,0,1,2.384,0,1.7,1.7,0,0,1,0,2.391L19.2,24.258a1.685,1.685,0,0,1-2.327.049L6.68,14.14a1.688,1.688,0,0,1,2.384-2.391Z" transform="translate(-6.188 -11.246)"/></svg>
                             </div>
 
                             {/* Sub menu */}
@@ -73,10 +74,10 @@ export default function Header () {
                 </div>
                 <div className="w-3/4">
                     <div>                    
-                        <span className="text-pred font-logotitle text-4xl font-bold ml-2">Exposition du centenaire de l'archevêché des églises  </span> 
+                        <span className="text-pred font-logotitle text-4xl font-bold ml-2">Centenaire de l'archevêché des églises Orthodoxes</span> 
                     </div>
                     <div> 
-                        <span className="text-pred font-logotitle text-4xl font-bold ml-2">Orthodoxes de tradition russe en Europe occidentale</span> 
+                        <span className="text-pred font-logotitle text-4xl font-bold ml-2"> de tradition russe en Europe occidentale</span> 
                     </div>
                 </div>
             </div>
