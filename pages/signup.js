e5fd74aeb68740491a48a6ce58b59786a2f1a82b
KenyZ <zkeny@outlook.fr>
2021-06-17 12:07:09 +0200

    signup has Header

--- pages/signup.js
+++ pages/signup.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Header from '../components/header';
+import Header from '../components/header/header';
 import SignupComp from '../components/signup';
+import { getMenu } from '../model/menu';
 
-export default function Login() {
+export default function Login({menu}) {
     return (
         <div>
-            <Header />
+            {menu && <Header menu={menu.data}/>}
             <SignupComp />
         </div>
     );
+}
+
+export async function getServerSideProps(context) {
+    const menu = await getMenu(context.locale);
+    
+    console.log(menu)
+    return {
+        props: {menu},
+    };
 }
