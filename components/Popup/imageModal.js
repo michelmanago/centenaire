05b54d2788b895cd71ba92f20b5f6969d9c2c4b1
Thibault MARTIN <developpeur.martin.thibault@gmail.com>
2021-08-05 13:43:11 +0200

    affichage en grand des images dans les page

--- components/Popup/imageModal.js
+++ components/Popup/imageModal.js
@@ -3,38 +3,49 @@ import Popup from "reactjs-popup";
 import IconClose from "../icons/IconClose";
 
 
-export default function ImageModal({ url, className }) {
-    const [open, setOpen] = useState(false);
+
+
+export default function ImageModal({url, className}){
+    const[open, setOpen] = useState(false);
+    console.log(className);
     return (
         <div>
             <div onClick={e => {
                 e.preventDefault();
                 setOpen(true);
             }}
-            >
+        >
                 <img src={url} className={className} />
+                
             </div>
-            <Popup
-                open={open}
-                onClose={setOpen}
-                className={'image-modal'}
-                closeOnEscape={false} // beacause this can not be prevented
-                closeOnDocumentClick={false} // beacause this event can not be prevented 
+            <div className="popup">
+            <div className="h-auto my-6 overflow-hidden bg-white rounded-xl md:max-w-full mb-9">
+ 
+        <Popup
+            open={open}
+            onClose={setOpen}
+            className={'image-modal'}
+            closeOnEscape={false} // beacause this can not be prevented
+            closeOnDocumentClick={false} // beacause this event can not be prevented4
+            
             >
-                <div className="p-1  bg-white border rounded ">
-                    {/* Close */}
-                    <button
+                  <div className="mt-6 p-1 flex flex-wrap justify-center ">
+                  
+                      {/* Close */}
+                      <button
                         onClick={() => setOpen(false)}
-                        className="absolute top-0 right-0 text-gray-700 hover:text-gray-800 ">
-                        <IconClose />
-                    </button>
-                    <img
-                        className="mt-6  "
-                        src={url}
-
-                    />
-                </div>
+                        className="absolute top-0 right-0 text-gray-700 hover:text-gray-800 flex flex-wrap ">
+                            <IconClose />
+                        </button>
+                        <img
+                            className=""
+                            src={url}
+                            
+                        />
+                  </div>
             </Popup>
+            </div>
+            </div>
         </div>
     )
 }
