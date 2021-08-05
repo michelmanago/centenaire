848befc93103baf134dbdbe3fb49d99eaa9c663a
Thibault MARTIN <developpeur.martin.thibault@gmail.com>
2021-08-05 14:58:25 +0200

    ragement de code imageModal

--- components/Popup/imageModal.js
+++ components/Popup/imageModal.js
@@ -5,8 +5,8 @@ import IconClose from "../icons/IconClose";
 
 
 
-export default function ImageModal({url, className}){
-    const[open, setOpen] = useState(false);
+export default function ImageModal({ url, className }) {
+    const [open, setOpen] = useState(false);
     console.log(className);
     return (
         <div>
@@ -14,38 +14,30 @@ export default function ImageModal({url, className}){
                 e.preventDefault();
                 setOpen(true);
             }}
-        >
-                <img src={url} className={className} />
-                
-            </div>
-            <div className="popup">
-            <div className="h-auto my-6 overflow-hidden bg-white rounded-xl md:max-w-full mb-9">
- 
-        <Popup
-            open={open}
-            onClose={setOpen}
-            className={'image-modal'}
-            closeOnEscape={false} // beacause this can not be prevented
-            closeOnDocumentClick={false} // beacause this event can not be prevented4
-            
             >
-                  <div className="mt-6 p-1 flex flex-wrap justify-center ">
-                  
-                      {/* Close */}
-                      <button
+                <img src={url} className={className} />
+            </div>
+
+            <Popup
+                open={open}
+                onClose={setOpen}
+                className={'image-modal'}
+                closeOnEscape={false} // beacause this can not be prevented
+                closeOnDocumentClick={false} // beacause this event can not be prevented4
+            >
+                <div className="mt-6 p-1 flex flex-wrap justify-center ">
+                    {/* Close */}
+                    <button
                         onClick={() => setOpen(false)}
                         className="absolute top-0 right-0 text-gray-700 hover:text-gray-800 flex flex-wrap ">
-                            <IconClose />
-                        </button>
-                        <img
-                            className=""
-                            src={url}
-                            
-                        />
-                  </div>
+                        <IconClose />
+                    </button>
+                    <img
+                        className=""
+                        src={url}
+                    />
+                </div>
             </Popup>
-            </div>
-            </div>
         </div>
     )
 }
