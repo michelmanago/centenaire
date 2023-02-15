1a9dfa851e9aae842835bea63029ebdc9465f9f5
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2023-02-15 08:28:04 +0100

    Remove unused code

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import getTests from '../../model/test';
-
-export default async (req, res) => {
-    const testData = await getTests();
-    res.statusCode = 200;
-    res.json({name: 'John Doe', testData: testData});
-};
