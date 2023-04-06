6132b7141fe9469cb14d24cf7a872d0c5715dafc
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2023-04-06 17:18:27 +0200

    Media: fix delete media call api

--- components/list-media/list-media-edit.js
+++ components/list-media/list-media-edit.js
@@ -1,286 +1,253 @@
 // libs
-import { useEffect, useState } from "react";
-import Popup from "reactjs-popup";
+import {useEffect, useState} from 'react';
+import Popup from 'reactjs-popup';
 import {useRouter} from 'next/router';
 
-
 // utils
-import { getMediaLink } from "../../utils/utils-serveur-image"
+import {getMediaLink} from '../../utils/utils-serveur-image';
 import fetchDeleteMedia from '../../utils/fetch/fetchDeleteMedia';
 import fetchUpdateMedia from '../../utils/fetch/fetchUpdateMedia';
-import { getFilenameFromPath, legendeAsArray, MEDIA_TYPES } from '../../utils/utils-media';
-import IconClose from "../icons/IconClose";
-
+import {getFilenameFromPath, legendeAsArray, MEDIA_TYPES} from '../../utils/utils-media';
+import IconClose from '../icons/IconClose';
 
 // styles
 const contentStyles = {
-    width: "85%",
-    height: "92%",
-    overflow: "auto",
-    borderRadius: ".2em",
-    background: "#fff",
-    boxShadow: "0 0 2px rgba(0, 0, 0, .2)"
-}
+    width: '85%',
+    height: '92%',
+    overflow: 'auto',
+    borderRadius: '.2em',
+    background: '#fff',
+    boxShadow: '0 0 2px rgba(0, 0, 0, .2)',
+};
 
-
-export default function ListMediaEdit({media, onClose, updateList}){
-
-
+export default function ListMediaEdit({media, onClose, updateList}) {
     // hooks
-    const {locales} = useRouter()
-    const defaultLegendes = locales.map(locale => ({locale, value: ""}))
+    const {locales} = useRouter();
+    const defaultLegendes = locales.map(locale => ({locale, value: ''}));
 
     // states
-    const [modified, setModified] = useState(false)
-    const [credit, setCredit] = useState(media ? media.credit : "")
+    const [modified, setModified] = useState(false);
+    const [credit, setCredit] = useState(media ? media.credit : '');
     const [legendes, setLegendes] = useState(
-        media ? (media.legende ? legendeAsArray(media.legende) : defaultLegendes) : defaultLegendes
-    )
-
+        media ? (media.legende ? legendeAsArray(media.legende) : defaultLegendes) : defaultLegendes,
+    );
 
     // helpers
     const renderMediaPreview = () => {
+        const media_link = getMediaLink(media.public_path);
 
-        const media_link = getMediaLink(media.public_path)
-        
-        switch(media.type){
+        switch (media.type) {
             case MEDIA_TYPES.IMAGE:
                 return (
-                    <div className="w-1/2 rounded border-2 border-gray-400">
+                    <div className="w-1/2 border-2 border-gray-400 rounded">
                         <img src={media_link} alt="" />
                     </div>
-                )
-            break;
+                );
+                break;
             case MEDIA_TYPES.VIDEO:
                 return (
-                    <div className="w-3/4 rounded border-2 border-gray-400">
+                    <div className="w-3/4 border-2 border-gray-400 rounded">
                         <video controls src={media_link} muted loop></video>
                     </div>
-                )
-            break;
+                );
+                break;
             case MEDIA_TYPES.AUDIO:
                 return (
-                    <div className="rounded border-2 border-gray-400">
+                    <div className="border-2 border-gray-400 rounded">
                         <audio className="w-full" src={media_link} controls loop></audio>
                     </div>
-                )
-            break;
+                );
+                break;
             case MEDIA_TYPES.DOCUMENT:
             default:
-                    return (
+                return (
                     <div className="">
-                        <a target="_blank" className="underline text-blue-500" href={media_link}>Voir le fichier</a>
+                        <a target="_blank" className="text-blue-500 underline" href={media_link}>
+                            Voir le fichier
+                        </a>
                     </div>
-                )
-            break;
-        
+                );
+                break;
         }
-
-    }
+    };
 
     // setters
     const setOneLegende = (value, index) => {
-
         const nextLegendes = legendes.map((legende, legendeIndex) => {
-
-            if(legendeIndex === index){
+            if (legendeIndex === index) {
                 return {
                     ...legende,
                     value,
-                }
+                };
             } else {
-                return legende
+                return legende;
             }
+        });
 
-        })
-
-        setLegendes(nextLegendes)
-    }
+        setLegendes(nextLegendes);
+    };
 
     // methods
-    
-    const onChangeCredit = event => {
-        setCredit(event.target.value)
 
-    }
+    const onChangeCredit = event => {
+        setCredit(event.target.value);
+    };
 
     const onUpdateMedia = async () => {
-
         const form = {
             credit,
             legende: legendes,
-        }
+        };
 
         try {
-
             // request update
-            const newMedia = await fetchUpdateMedia(media.id, form)
+            const newMedia = await fetchUpdateMedia(media.id, form);
 
             // update list
             updateList(list => {
-
                 return list.map(mediaItem => {
-
-                    if(mediaItem.id === media.id){
-                        return newMedia
+                    if (mediaItem.id === media.id) {
+                        return newMedia;
                     } else {
-                        return mediaItem
+                        return mediaItem;
                     }
-
-                })
-
-            })
+                });
+            });
 
             // show feedback
-            setModified(true)
-            setTimeout(() => setModified(false), 1500)
-
+            setModified(true);
+            setTimeout(() => setModified(false), 1500);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-
-    }
+    };
 
     const onRemoveMedia = async () => {
-
         // Prevent miss click
-        if(confirm("Êtes vous sûr de vouloir supprimer définitivement l'image ?")){
-
+        if (confirm("Êtes vous sûr de vouloir supprimer définitivement l'image ?")) {
             // fetch DELETE
-            const deleted = await fetchDeleteMedia(media.id)
+            const deleted = await fetchDeleteMedia(media);
 
-            if(deleted){
-                
+            if (deleted) {
                 // remove from list
                 updateList(list => {
-                    return list.filter(mediaItem => mediaItem.id !== media.id)
-                })
+                    return list.filter(mediaItem => mediaItem.id !== media.id);
+                });
 
                 // close
-                onClose()
-
+                onClose();
             } else {
-                alert("Could not delete this media.")
+                alert('Could not delete this media.');
             }
-
         }
-
-    }
+    };
 
     // others
-    const filename = media && getFilenameFromPath(media)
+    const filename = media && getFilenameFromPath(media);
 
     useEffect(() => {
-
         return () => {
-            onClose()
-        }
-
-    }, [])
+            onClose();
+        };
+    }, []);
 
     useEffect(() => {
-
-        if(media){
-            setCredit(media.credit)
-            setLegendes((media.legende ? legendeAsArray(media.legende) : defaultLegendes) )
+        if (media) {
+            setCredit(media.credit);
+            setLegendes(media.legende ? legendeAsArray(media.legende) : defaultLegendes);
         }
-
-    }, [media])
+    }, [media]);
 
     return (
-        <Popup
-            lockScroll={true}
-            open={!!media}
-            contentStyle={contentStyles}
-            onClose={onClose}
-            modal
-        >
-            {
-                media && (
-                    <div className="py-10 px-6">
+        <Popup lockScroll={true} open={!!media} contentStyle={contentStyles} onClose={onClose} modal>
+            {media && (
+                <div className="px-6 py-10">
+                    {/* Close */}
+                    <button onClick={onClose} className="absolute text-gray-700 hover:text-gray-800 right-2 top-2">
+                        <IconClose />
+                    </button>
 
-                        
-                        {/* Close */}
-                        <button onClick={onClose} className="absolute text-gray-700 hover:text-gray-800 right-2 top-2">
-                            <IconClose/>
-                        </button>
+                    {/* Type */}
+                    <p>
+                        <span>Ficher de type : </span>
+                        <span className="inline-block px-4 py-1 mb-5 text-sm text-purple-800 bg-purple-200 border-2 border-purple-300 rounded">
+                            {media.type}
+                        </span>
+                    </p>
 
-                        {/* Type */}
-                        <p>
-                            <span>Ficher de type : </span>
-                            <span className="inline-block mb-5 bg-purple-200 px-4 py-1 text-sm text-purple-800 rounded border-2 border-purple-300">{media.type}</span>
-                        </p>
+                    {/* Preview */}
+                    <div className="w-2/3">{renderMediaPreview()}</div>
 
-                        {/* Preview */}
-                        <div className="w-2/3">
-                            {
-                                renderMediaPreview()
-                            }
-                        </div>
+                    {/* Filename */}
+                    <p className="mt-3 text-sm text-gray-600">
+                        Nom du fichier : <em>{filename}</em>
+                    </p>
 
-                        {/* Filename */}
-                        <p className="text-sm mt-3 text-gray-600">Nom du fichier : <em>{filename}</em></p>
+                    {/* Remove */}
+                    <button onClick={onRemoveMedia} className="text-red-400 underline">
+                        Supprimer définitivement
+                    </button>
 
-                        {/* Remove */}
-                        <button onClick={onRemoveMedia} className="text-red-400 underline">Supprimer définitivement</button>
+                    {/* Association */}
 
-                        {/* Association */}
+                    {/* Form */}
+                    <div className="mt-4">
+                        {/* Crédit */}
+                        <InputText label="Crédit :" value={credit} onChange={onChangeCredit} />
 
-
-                        {/* Form */}
-                        <div className="mt-4">
-
-                            {/* Crédit */}
+                        {/* Legendes */}
+                        {legendes.map(({locale}, index) => (
                             <InputText
-                                label="Crédit :"
-                                value={credit}
-                                onChange={onChangeCredit}
+                                key={locale}
+                                label={`Légende ${locale} :`}
+                                value={legendes[index].value}
+                                onChange={e => setOneLegende(e.target.value, index)}
                             />
+                        ))}
 
-                            {/* Legendes */}
+                        {/* Submit */}
+                        <div className="relative inline-block border">
+                            {/* Button */}
+                            <button
+                                type="button"
+                                onClick={onUpdateMedia}
+                                className="px-3 py-1 text-white bg-green-500 rounded hover:bg-green-600"
+                            >
+                                Sauvegarder le média
+                            </button>
+
+                            {/* Animation */}
                             {
-                                legendes.map(({locale}, index) => (
-                                    <InputText
-                                        key={locale}
-                                        label={`Légende ${locale} :`}
-                                        value={legendes[index].value}
-                                        onChange={e => setOneLegende(e.target.value, index)}
-                                    />
-                                ))
+                                // hasModified && (
+                                //     <span className="absolute top-0 right-0 flex w-3 h-3 -mt-1 -mr-1">
+                                //         <span className="absolute inline-flex w-full h-full bg-blue-400 rounded-full opacity-75 animate-ping"></span>
+                                //         <span className="relative inline-flex w-3 h-3 bg-blue-500 rounded-full"></span>
+                                //     </span>
+                                // )
                             }
-
-                            {/* Submit */}
-                            <div className="relative border inline-block">
-                                
-                                {/* Button */}
-                                <button type="button" onClick={onUpdateMedia} className="bg-green-500 hover:bg-green-600 px-3 py-1 text-white rounded">Sauvegarder le média</button>
-                                
-                                {/* Animation */}
-                                {
-                                    // hasModified && (
-                                    //     <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
-                                    //         <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>
-                                    //         <span className="relative inline-flex rounded-full h-3 w-3 bg-blue-500"></span>
-                                    //     </span>
-                                    // )
-                                }
-                            </div>
-                            
-                            {/* Feedback */}
-                            {modified &&  <span className="ml-2 text-sm text-green-500">Modifié ✓</span>}
                         </div>
 
+                        {/* Feedback */}
+                        {modified && <span className="ml-2 text-sm text-green-500">Modifié ✓</span>}
                     </div>
-                )
-            }
+                </div>
+            )}
         </Popup>
-    )
-
+    );
 }
 
 const InputText = ({placeholder, value, label, onChange, id}) => (
     <div className="mb-4">
-        <label className="block mb-1 font-medium" htmlFor={id}>{label}</label>
-        <input className="block w-full px-1 py-1 border rounded " type="text"id={id} placeholder={placeholder} onChange={onChange} value={value}/>
+        <label className="block mb-1 font-medium" htmlFor={id}>
+            {label}
+        </label>
+        <input
+            className="block w-full px-1 py-1 border rounded "
+            type="text"
+            id={id}
+            placeholder={placeholder}
+            onChange={onChange}
+            value={value}
+        />
     </div>
-)
+);
--- utils/fetch/fetchDeleteMedia.js
+++ utils/fetch/fetchDeleteMedia.js
@@ -1,30 +1,27 @@
-import { getServerImageEndpoint } from "../utils-serveur-image";
+import {getServerImageEndpoint} from '../utils-serveur-image';
 
-export default async function fetchDeleteMedia(mediaId){
-
+export default async function fetchDeleteMedia(media) {
     try {
-
-        let results = null
+        let results = null;
+        let mediaNameSplit = media.public_path.split('/');
+        let mediaName = mediaNameSplit[mediaNameSplit.length - 1];
 
         // Fetch
-        const response = await fetch(getServerImageEndpoint() + "/" + mediaId , {
-            method: "DELETE",
-        })
+        const response = await fetch(getServerImageEndpoint() + '/' + mediaName, {
+            method: 'DELETE',
+        });
 
         // Decode
-        if(response.ok){
-            results = await response.json()
+        if (response.ok) {
+            results = await response.json();
         } else {
             throw new Error(response.statusText);
         }
 
         // Receive results
-        return true
-
+        return true;
     } catch (error) {
-        console.log("FetchDeleteMedia", error)
-        return false
+        console.log('FetchDeleteMedia', error);
+        return false;
     }
-
-
 }
