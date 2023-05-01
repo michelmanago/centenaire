808ae44b150951ef0a583803afd22719355e7f69
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2023-05-01 09:56:43 +0200

    Slug: clean slug add character to escape

--- utils/cleanForSlug.js
+++ utils/cleanForSlug.js
@@ -1,23 +1,21 @@
+export default function cleanForSlug(str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
 
-export default function cleanForSlug(str)
-{
-	str = str.replace(/^\s+|\s+$/g, ''); // trim
-	str = str.toLowerCase();
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñçěščřžýúůďťň·/_,:;'";
+    var to = 'aaaaeeeeiiiioooouuuuncescrzyuudtn-------';
 
-	// remove accents, swap ñ for n, etc
-	var from = "àáäâèéëêìíïîòóöôùúüûñçěščřžýúůďťň·/_,:;";
-	var to   = "aaaaeeeeiiiioooouuuuncescrzyuudtn------";
+    for (var i = 0, l = from.length; i < l; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
 
-	for (var i=0, l=from.length ; i<l ; i++)
-	{
-		str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-	}
+    str = str
+        .replace('.', '-') // replace a dot by a dash
+        .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by a dash
+        .replace(/-+/g, '-') // collapse dashes
+        .replace(/\//g, ''); // collapse all forward-slashes
 
-	str = str.replace('.', '-') // replace a dot by a dash 
-		.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-		.replace(/\s+/g, '-') // collapse whitespace and replace by a dash
-		.replace(/-+/g, '-') // collapse dashes
-		.replace( /\//g, '' ); // collapse all forward-slashes
-
-	return str;
+    return str;
 }
